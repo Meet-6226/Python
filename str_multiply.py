53b1660330be70867c47ccabd20e5a8e61089fb4
Meet Alshi <meetalshidevendra@gmail.com>
2025-01-25 23:53:51 +0530

    Add files via upload

---
+++ str_multiply.py
@@ -0,0 +1,5 @@
+#Write a function that takes a string s and an integer n, and returns the string repeated n times.
+
+string=str(input("Enter a string"))
+num=int(input("Enter a number"))
+print(string*num)
